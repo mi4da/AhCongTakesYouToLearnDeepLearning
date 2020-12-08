ef44eb524b8a8d54f3214a388ade4ea36d54bb61
mi4da <55870943+mi4da@users.noreply.github.com>
2020-12-09 00:42:33 +0800

    win
    神经网络拟合器完成

--- code/data_create.py
+++ code/data_create.py
@@ -3,22 +3,28 @@ import numpy as np
 
 
 class DataCreater:
-    def __init__(self,num):
+    def __init__(self, num):
         self.num = num
-        self.x = np.arange(0, 5, 5/num )
-        self.y =5 * np.sin(self.x)  + 3
+        self.x = np.arange(0, 5, 5 / num)
+        self.y = (self.x-2.5) ** 2
 
     def normliza_x(self):
-        self.x = self.x/ max(self.x)
+        self.x = self.x / max(self.x)
+
+    def normlize_xy(self):
+        self.x = self.x / max(self.x)
         self.y = self.y / max(self.y)
 
     def get_data(self):
         return (self.x, self.y)
+    def make_random(self):
+        self.y = self.y + np.random.randint(1,5,len(self.x))
 
     def plot_data(self):
-        plt.scatter(self.x,self.y,color = 'b')
-        plt.plot(self.x, self.y, color='b')
+        plt.scatter(self.x, self.y, color='b')
+        # plt.plot(self.x, self.y, color='b')
         plt.show()
+
 
 if __name__ == '__main__':
     demo = DataCreater(100)
